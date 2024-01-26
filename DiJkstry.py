7fc3b823637e38e16286d5b5291bcb0f7cc02c11
mariuszkoczuminski <149315354+mariuszkoczuminski@users.noreply.github.com>
2024-01-26 17:20:13 +0100

    Update DiJkstry.py

--- DiJkstry.py
+++ DiJkstry.py
@@ -2,7 +2,7 @@
 import heapq
 
 def dij(graph, start):
-    # Inicjalizacjemy odleglość i kolejki
+    
     odleglosci = {wierzcholek: float('infinity') for wierzcholek in graph}
     odleglosci[start] = 0
     kolejka_priorytetowa = [(0, start)]
@@ -10,15 +10,15 @@ def dij(graph, start):
     while kolejka_priorytetowa:
         aktualna_odleglosc, aktualny_wierzcholek = heapq.heappop(kolejka_priorytetowa)
 
-        # Jeśli już znaleźliśmy krótszą ścieżkę do danego wierzchołka, pomijamy go
+        
         if aktualna_odleglosc > odleglosci[aktualny_wierzcholek]:
             continue
 
-        # Badamy sąsiadów
+    
         for sasiad, waga in graph[aktualny_wierzcholek].items():
             odleglosc = aktualna_odleglosc + waga
 
-            # Jeśli znaleziono krótszą ścieżkę, aktualizujemy odległość i dodajemy do kolejki
+            
             if odleglosc < odleglosci[sasiad]:
                 odleglosci[sasiad] = odleglosc
                 heapq.heappush(kolejka_priorytetowa, (odleglosc, sasiad))
